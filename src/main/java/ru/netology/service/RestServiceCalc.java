118471f76aafc387373ec7a57caf310a38cd4b13
Nikita G <jobnikitka152rus@gmail.com>
2023-05-17 18:41:52 +0300

    error correction service

--- src/main/java/ru/netology/service/RestServiceCalc.java
+++ src/main/java/ru/netology/service/RestServiceCalc.java
@@ -7,15 +7,11 @@ public class RestServiceCalc {
         //threshold - осталось
         int count = 0; // счётчик месяцев отдыха
         int money = 0; // количество денег на счету
-        int moneyToRest = 0; // затраты на отдых
         for (int month = 1; month <= 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
-                moneyToRest = (money - expense) - ((money - expense) / 3);
-                System.out.println("Месяц " + month + ". Денег на счету " + money + ". Буду отдыхать. Потратил " + expense + ", затем еще " + moneyToRest);
                 count++; // увеличиваем счётчик месяцев отдыха
-                money = (money - expense) / 3;
+                money =- expense / 3;
             } else {
-                System.out.println("Месяц " + month + ". Денег на счету " + money + ". Придется поработать. Заработал " + income + ". Потратил " + expense);
                 money = money + income - expense;
 
             }
